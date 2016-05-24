f17c2e8fefe385366250635656f70ed246a603e5
Pavel Neizhmak <PavelN@playtika.com>
2016-05-24 16:48:22 +0300

    add support to view parsed response

--- client/src/main/webapp/core.js
+++ client/src/main/webapp/core.js
@@ -8,17 +8,21 @@ core.getServiceUrl = function (baseUrl) {
 
 $(function() {
 
-    var baseUrl = "http://localhost:8081/"
+    var baseUrl = "http://localhost:8081/";
 
-    $('#smart-search-form').submit(function(e) {
+    $('#smart-search-form').submit(function (e) {
         e.preventDefault();
+        var textarea = $('#smart-search-form').find('textarea#result');
         $.ajax({
             url: core.getServiceUrl(baseUrl),
             type: 'get',
-            success: function(data) {
-                var textarea = $('#smart-search-form textarea#result');
-                textarea.val(JSON.stringify(data));
+            success: function (data) {
+                textarea.val(data);
                 console.log(data);
+            },
+            error: function (data) {
+                textarea.val(data.valueOf().responseText);
+                console.log(data.valueOf());
             }
         });
     });
