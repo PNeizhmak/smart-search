fdc3e859961ee0b76641774fd5a4578a42d106b8
Pavel Neizhmak <PavelN@playtika.com>
2016-07-13 12:43:58 +0300

    add prop file for ds

--- src/main/java/com/config/SmartSearchConfiguration.java
+++ src/main/java/com/config/SmartSearchConfiguration.java
@@ -2,9 +2,12 @@ package com.config;
 
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,8 +18,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @EnableWebMvc
 @Configuration
+@PropertySource("classpath:database.properties")
 @ComponentScan(basePackages = "com")
 public class SmartSearchConfiguration extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
 
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
@@ -25,8 +32,15 @@ public class SmartSearchConfiguration extends WebMvcConfigurerAdapter {
 
     @Bean
     public DriverManagerDataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource("jdbc:mysql://localhost:3306/user_db", "root", "root");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+
+        final String dbUrl = env.getRequiredProperty("database.url");
+        final String dbUser = env.getRequiredProperty("database.user");
+        final String dbPass = env.getRequiredProperty("database.password");
+        final String dbDriver = env.getRequiredProperty("database.driver");
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(dbUrl, dbUser, dbPass);
+        dataSource.setDriverClassName(dbDriver);
+
         return dataSource;
     }
 
