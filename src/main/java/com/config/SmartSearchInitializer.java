5208c4ecf07ea038fdc1b52f975e57464523f436
Pavel Neizhmak <PavelN@playtika.com>
2016-08-12 17:08:00 +0300

    add encoding filter

--- src/main/java/com/config/SmartSearchInitializer.java
+++ src/main/java/com/config/SmartSearchInitializer.java
@@ -1,11 +1,13 @@
 package com.config;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /**
@@ -17,6 +19,11 @@ public class SmartSearchInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
         rootContext.register(SmartSearchConfiguration.class);
+
+        FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        filter.setInitParameter("encoding", "UTF-8");
+        filter.setInitParameter("forceEncoding", "true");
+        filter.addMappingForUrlPatterns(null, true, "/*");
 
         servletContext.addListener(new ContextLoaderListener(rootContext));
 
